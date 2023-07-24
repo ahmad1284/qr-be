e7ff529b50d4b3b18b8c70deb5eea4637ab052bb
Ahmad Abdullah <ahmadnofly@gmail.com>
2023-07-24 13:42:21 +0300

    add env

--- qr.local.js
+++ qr.local.js
@@ -2,7 +2,7 @@ const express = require('express');
 const qrcode = require('qrcode');
 const cors = require('cors')
 // const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 
